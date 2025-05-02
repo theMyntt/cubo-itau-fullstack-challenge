55d343787d5b50ff0bfed2064d69c061d643d054
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com.br>
2025-05-02 20:14:30 -0300

    feat: db context

---
+++ App_Data/DatabaseContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using CuboFullStackChallenge.App_Models;
+
+namespace CuboFullStackChallenge.App_Data
+{
+    public class DatabaseContext : DbContext
+    {
+        public DbSet<UserModel> Users { get; protected set; }
+
+        public DatabaseContext()
+            : base(ConfigurationManager.ConnectionStrings["SqlServer"].ConnectionString)
+        {
+        }
+    }
+}
