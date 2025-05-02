02583b38f882200a1614077a92d7b0b0782be465
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com.br>
2025-05-02 20:09:13 -0300

    refactor: saving model on App_Models folder

--- App_Models/UserModel.cs
+++ App_Models/UserModel.cs
@@ -5,7 +5,7 @@ using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
-namespace CuboFullStackChallenge.Models
+namespace CuboFullStackChallenge.App_Models
 {
     [Table("Users")]
     public class UserModel
