050d9868f876e792a656eca112b5f3eb41b6ed5c
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com.br>
2025-05-02 20:07:37 -0300

    chore: user model

---
+++ Models/UserModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace CuboFullStackChallenge.Models
+{
+    [Table("Users")]
+    public class UserModel
+    {
+        [Key]        
+        [Column(nameof(Id))]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Column(nameof(FirstName))]
+        public string FirstName { get; set; }
+
+        [Column(nameof(LastName))]
+        public string LastName { get; set; }
+
+        [Column(nameof(Participation))]
+        public int Participation { get; set; }
+
+        [Column(nameof(Email))]
+        public string Email { get; set; }
+
+        [Column(nameof(Password))]
+        public string Password { get; set; }
+    }
+}
